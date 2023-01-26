bb7fa940267387adac51134c1bc5778879e58b89
chris abdo <chris.j.abdo@gmail.com>
2023-01-26 13:33:51 -0500

    ADDED: text changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,7 +150,7 @@ const Home: NextPage = () => {
                 <>
                   <div>
                     <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
-                      Your generated bios
+                      Your generated pickup lines
                     </h2>
                   </div>
                   <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
